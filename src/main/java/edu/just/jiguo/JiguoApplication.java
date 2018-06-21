4187050ef9939c7512f466a19d8542170092966c
luwenhe12345 <luwenhe12345@126.com>
2018-06-21 19:33:58 +0800

    第一次提交

---
+++ src/main/java/edu/just/jiguo/JiguoApplication.java
@@ -0,0 +1,12 @@
+package edu.just.jiguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiguoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiguoApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/just/jiguo/JiguoApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.just.jiguo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiguoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
